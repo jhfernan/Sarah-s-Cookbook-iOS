c6fc85aff6caccb48f2f485d966ce962e38b4ba7
Jonathan Fernandes <jhfernan@mail.usf.edu>
2020-06-22 00:53:52 +0800

    Add project assets

---
+++ Sarahs Cookbook/Model/RecipeData.swift
@@ -0,0 +1,14 @@
+//
+//  RecipeData.swift
+//  Sarahs Cookbook
+//
+//  Created by Jonathan Fernandes on 6/21/20.
+//  Copyright © 2020 Jonathan Fernandes. All rights reserved.
+//
+
+import Foundation
+
+struct RecipeData: Codable {
+    let name: String
+    let time: Int
+}
