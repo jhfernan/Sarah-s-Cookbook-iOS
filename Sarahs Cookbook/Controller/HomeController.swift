cd48abab1dfb05f6c63698d41451c983a69630c5
Jonathan Fernandes <jhfernan@mail.usf.edu>
2020-06-22 11:18:13 +0800

    Add Register, Login, Home views and setup main menu

---
+++ Sarahs Cookbook/Controller/HomeController.swift
@@ -0,0 +1,17 @@
+//
+//  HomeController.swift
+//  Sarahs Cookbook
+//
+//  Created by Jonathan Fernandes on 6/22/20.
+//  Copyright © 2020 Jonathan Fernandes. All rights reserved.
+//
+
+import UIKit
+
+class HomeController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+}
